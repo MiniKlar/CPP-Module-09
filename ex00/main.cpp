098586f540e9f37138a502681aab21a0a849ec71
MiniKlar <omontloris@gmail.com>
2026-01-20 05:16:16 +0100

    - Added main function implementation with BitcoinDatabase and InputDatabase initialization

---
+++ ex00/main.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/17 02:55:36 by lomont            #+#    #+#             */
+/*   Updated: 2026/01/20 05:16:04 by lomont           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "BitcoinExchange.hpp"
+
+int	main(int argc, char** argv) {
+	BitcoinDatabase _class;
+	InputDatabase	_input(argv[argc - 1]);
+
+	return (0);
+}
