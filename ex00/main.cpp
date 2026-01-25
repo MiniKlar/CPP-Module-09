0c790d4a44242b97038bbb204eba205223128d50
MiniKlar <omontloris@gmail.com>
2026-01-25 18:13:14 +0100

    - Updated main.cpp to correct the constructor call for BitcoinExchange and revised the file update timestamp.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/17 02:55:36 by lomont            #+#    #+#             */
-/*   Updated: 2026/01/25 15:48:15 by lomont           ###   ########.fr       */
+/*   Updated: 2026/01/25 17:00:50 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 int	main(int argc, char** argv) {
 	if (argc == 2) {
 		BitcoinDatabase _class;
-		InputDatabase	_input(argv[argc - 1], _class);
-		//BitcoinExchange	_exchange(_class, _input);
+		BitcoinExchange	_exchange(argv[argc - 1], _class);
 		return (0);
 	}
 	std::cerr << "Error: could not open file." << std::endl;
