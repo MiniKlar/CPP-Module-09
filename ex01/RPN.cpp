c865fa120f72ce4eecf22fa89ad7f5f9c9e0efab
MiniKlar <omontloris@gmail.com>
2026-02-01 23:09:33 +0100

    - Implemented RPN class with parsing, execution, and error handling methods

--- ex01/RPN.cpp
+++ ex01/RPN.cpp
@@ -6,14 +6,63 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 00:13:42 by lomont            #+#    #+#             */
-/*   Updated: 2026/01/30 16:53:44 by lomont           ###   ########.fr       */
+/*   Updated: 2026/02/01 23:06:26 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
+//canonical form
+
+RPN::RPN( int argc, char** argv ) {
+	//std::cout << "RPN default constructor called" << std::endl;
+	this->str = argv[argc - 1];
+	if (!this->str[0])
+		this->ft_error();
+	parsing();
+	execution();
+	return ;
+}
+
+RPN::RPN( const RPN& src) : s(src.s) {
+	//std::cout << RPN copy constructor called" << std::endl;
+	return ;
+}
+
+RPN& RPN::operator=( const RPN& other ) {
+	//std::cout << "RPN assignement operator called" << std::endl;
+	if (&other != this) {
+		this->s = other.s;
+	}
+	return ( *this );
+}
+
+RPN::~RPN( void ) {
+	//std::cout << "RPN Destructor called" << std::endl;
+	return ;
+}
+
+void RPN::parsing( void ) {
+	for (size_t i = 0; str[i]; i++) {
+		if (i % 2 == 1) {
+			if (str[i] != ' ')
+				this->ft_error();
+	}
+	else if (!isdigit(str[i]) && !checkOperator(str[i]) )
+		this->ft_error();
+	if (str[i + 1] == '\0') {
+		if (!checkOperatorNumbers(str[i], true))
+			this->ft_error();
+	}
+	checkOperatorNumbers(str[i], false);
+}
+if (!checkexpression(this->str)) {
+	this->ft_error();
+}
+}
+
 //function to check if char is */+-
-bool	checkOperator(char c) {
+bool	RPN::checkOperator(char c) {
 
 if (c == '*' || c == '/' || c == '+' || c == '-')
 		return ( true );
@@ -21,7 +70,7 @@ if (c == '*' || c == '/' || c == '+' || c == '-')
 }
 
 //Function to calcul the numbers of operators & operand to check if operators - 1 = operand
-bool	checkOperatorNumbers(char c, bool lastChar) {
+bool	RPN::checkOperatorNumbers(char c, bool lastChar) {
 	static	size_t numbers = 0, operators = 0;
 
 	if (c == ' ')
@@ -38,7 +87,7 @@ bool	checkOperatorNumbers(char c, bool lastChar) {
 }
 
 //Function to calcul the operation depending on the operand
-int	calculation(std::stack<int>& stack, char _operator) {
+int	RPN::calculation(std::stack<int>& stack, char _operator) {
 	int		n = 0;
 	int		m = 0;
 
@@ -59,4 +108,46 @@ int	calculation(std::stack<int>& stack, char _operator) {
 			return (m * n);
 	}
 	return (-1);
+}
+
+bool RPN::checkexpression(const char* str) {
+	size_t i = 0;
+	size_t stackSize = 0;
+
+	while (str[i]) {
+		if (str[i] == ' ')
+			i++;
+		if (isdigit(str[i]))
+			stackSize++;
+		else {
+			if (stackSize < 2)
+				return (false);
+			stackSize = stackSize - 1;
+		}
+		i++;
+	}
+	return (true);
+}
+
+void	RPN::ft_error( void ) {
+	std::cerr << "Error" << std::endl;
+	exit (1);
+}
+
+void	RPN::execution( void ) {
+	size_t	j = 0;
+	int 	result = 0;
+
+	while (str[j]) {
+		if (str[j] == ' ')
+			j++;
+		if (isdigit(str[j]))
+			this->s.push(str[j] - '0');
+		else {
+			result = calculation(this->s, str[j]);
+			this->s.push(result);
+		}
+		j++;
+	}
+	std::cout << this->s.top() << std::endl;
 }
--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 00:13:45 by lomont            #+#    #+#             */
-/*   Updated: 2026/01/30 16:49:46 by lomont           ###   ########.fr       */
+/*   Updated: 2026/02/01 23:06:34 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,28 @@
 
 #include <iostream>
 #include <stack>
+#include <cstdlib>
 
-bool	checkOperator(char c);
-bool	checkOperatorNumbers(char c, bool lastChar);
-int		calculation(std::stack<int>& stack, char _operator);
+class RPN
+{
+	private:
+		std::stack<int>	s;
+		const char*		str;
+	public:
+		//Canonical form
+		RPN( int argc, char** argv );
+		RPN( const RPN& src );
+		RPN& operator=( const RPN& other );
+		~RPN( void );
+
+		//Member functions
+		void	ft_error( void );
+		void	parsing( void );
+		void	execution( void );
+		bool	checkOperator(char c);
+		bool	checkOperatorNumbers(char c, bool lastChar);
+		bool	checkexpression(const char* str);
+		int		calculation(std::stack<int>& stack, char _operator);
+};
 
 #endif
