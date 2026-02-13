776ec6b1af48abe5ba9663a29f1a1b11706c3c5e
MiniKlar <omontloris@gmail.com>
2026-02-13 03:29:22 +0100

    - Refactored main function to utilize PmergeMe class for sorting and output results

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 16:51:22 by lomont            #+#    #+#             */
-/*   Updated: 2026/02/11 17:32:51 by lomont           ###   ########.fr       */
+/*   Updated: 2026/02/12 03:06:35 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,44 @@
 
 int	main( int argc, char** argv) {
 	if (argc >= 2) {
-		std::deque<int> container;
-		for (int i = 1; i < argc; i++) {
-			addNumber(&container, parseNumbers(argv[i]));
+		PmergeMe a(argc, argv);
+		std::deque<int> result = a.fordJohnson(a.getList());
+		
+		std::cout << "Sorted: ";
+		for (std::deque<int>::iterator it = result.begin(); it != result.end(); it++) {
+			std::cout << *it << " ";
 		}
-		for (std::deque<int>::iterator it = container.begin(); it != container.end(); it++) {
-			std::cout << *it << std::endl;
-		}
+		std::cout << std::endl;
 	}
 	else
 		return (1);
 	return (0);
 }
+
+		// std::deque<int> container;
+		// std::deque<int> winner;
+
+		// for (std::deque<int>::iterator it = container.begin(); it != container.end(); it++) {
+		// 	std::cout << *it << " ";
+		// }
+		// std::cout << std::endl;
+		// //boucle (bientôt récursive) pour trouver les gagnants
+		// for (std::deque<int>::iterator it = container.begin(); it != container.end();) {
+		// 	//faire des pas de 2 pour comparer 2 nombres, prendre le gagnant et le placer dans une autre deque
+		// 	int a = *it;
+		// 	//check avant si size pair ou impair pour gérer cas seul
+		// 	if (++it == container.end())
+		// 		break;
+		// 	int b = *(it);
+		// 	if (b > a) {
+		// 		winner.push_back(b);
+		// 		it = container.erase(it);
+		// 	}
+		// 	else {
+		// 		winner.push_back(a);
+		// 		it = container.erase(--it);
+		// 		it++;
+		// 	}
+		// }
+
+		// printResult(container, winner);
