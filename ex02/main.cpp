02e773856ea69e024cb8b53a59c0db12818bc852
MiniKlar <omontloris@gmail.com>
2026-02-14 05:32:12 +0100

    - Refactored main function to improve timing output and removed commented code

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 16:51:22 by lomont            #+#    #+#             */
-/*   Updated: 2026/02/12 03:06:35 by lomont           ###   ########.fr       */
+/*   Updated: 2026/02/14 05:26:44 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,18 @@ int	main( int argc, char** argv) {
 	if (argc >= 2) {
 		PmergeMe a(argc, argv);
 		std::deque<int> result = a.fordJohnson(a.getList());
-		
+		double start = a.get_time();
+		a.set_time();
+		std::cout << std::fixed << std::setprecision(5) << "Time to process a range of " << argc - 1 << " elements with std::deque : " << a.get_time() - start << " us" << std::endl;
 		std::cout << "Sorted: ";
 		for (std::deque<int>::iterator it = result.begin(); it != result.end(); it++) {
 			std::cout << *it << " ";
 		}
 		std::cout << std::endl;
+		bool sorted = std::is_sorted(result.begin(), result.end());
+		std::cout << sorted;
 	}
 	else
 		return (1);
 	return (0);
 }
-
-		// std::deque<int> container;
-		// std::deque<int> winner;
-
-		// for (std::deque<int>::iterator it = container.begin(); it != container.end(); it++) {
-		// 	std::cout << *it << " ";
-		// }
-		// std::cout << std::endl;
-		// //boucle (bientôt récursive) pour trouver les gagnants
-		// for (std::deque<int>::iterator it = container.begin(); it != container.end();) {
-		// 	//faire des pas de 2 pour comparer 2 nombres, prendre le gagnant et le placer dans une autre deque
-		// 	int a = *it;
-		// 	//check avant si size pair ou impair pour gérer cas seul
-		// 	if (++it == container.end())
-		// 		break;
-		// 	int b = *(it);
-		// 	if (b > a) {
-		// 		winner.push_back(b);
-		// 		it = container.erase(it);
-		// 	}
-		// 	else {
-		// 		winner.push_back(a);
-		// 		it = container.erase(--it);
-		// 		it++;
-		// 	}
-		// }
-
-		// printResult(container, winner);
