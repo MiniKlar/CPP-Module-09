f319e1f7de0339af93aca40c56ea357bc5443afc
MiniKlar <omontloris@gmail.com>
2026-02-15 03:22:47 +0100

    - Refactored PmergeMe class: update constructor, add vector support, and rename functions for clarity

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,115 +6,60 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 16:57:25 by lomont            #+#    #+#             */
-/*   Updated: 2026/02/14 05:31:42 by lomont           ###   ########.fr       */
+/*   Updated: 2026/02/15 03:16:50 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
-PmergeMe::PmergeMe( int argc, char** argv ) : sav(-1) {
-	std::cout << "Default PmergeMe constructor called" << std::endl;
-	for (int i = 1; i < argc; i++)
-		addNumber(&this->list, parseNumbers(argv[i]));
+//Canonical form
+
+PmergeMe::PmergeMe( int argc, char** argv ) {
+	//std::cout << "Default PmergeMe constructor called" << std::endl;
+	for (int i = 1; i < argc; i++) {
+		ft_addNumber(&this->list, &this->Vlist, ft_parseNumbers(argv[i]));
+	}
 	if (gettimeofday(&_time, NULL) == -1)
 		exit (1);
 	return ;
 }
 
 PmergeMe::~PmergeMe( void ) {
-	std::cout << "PmergeMe destructor called" << std::endl;
+	//std::cout << "PmergeMe destructor called" << std::endl;
 	return ;
 }
 
-int		parseNumbers( const char* str) {
-	double	number;
+//missing 2 functions
 
-	if (!isNumber(str))
-		printError("It's not a number");
-	number = atof(str);
-	if (hasOverflow(number)) {
-		printError("Overflow error");
-	}
-	return (static_cast<int>(number));
-}
-
-bool	hasOverflow(double n) {
-	if (n > INT_MAX) {
-		return (true);
-	}
-	return (false);
-}
-
-bool isNumber(const char* str) {
-	for (size_t i = 0; i < strlen(str); i++) {
-		if (str[0] == '-')
-			printError("Number negative");
-		if (!isnumber(str[i]))
-			return (false);
-	}
-	return (true);
-}
-
-void	addNumber(std::deque<int>* c, int n) {
-	c->push_back(n);
-	return ;
-}
-
-void	printError(std::string Error) {
-	std::cerr << Error << ", exiting program..." << std::endl; //for debug purpose only, for correction please put "Error"
-	exit(1);
-}
-
-void	PmergeMe::printResult( void ) {
-	int i = 1;
-	for (std::deque<std::pair<int, int> >::iterator it = this->winners.begin(); it != this->winners.end(); it++) {
-		std::cout << "a" << i << " = " << it->first << " b" << i << " = " << it->second << std::endl;
-		i++;
-	}
-	std::cout << "Voici la valeur save = " << this->sav << std::endl;
-}
-
-std::deque<int>& PmergeMe::getList( void ) {
+std::deque<int>&	PmergeMe::ft_getList( void ) {
 	return (this->list);
 }
 
-double	PmergeMe::get_time( void ) {
-	return (static_cast<double>(this->_time.tv_sec) * 1000000.0 + this->_time.tv_usec);
+std::vector<int>&	PmergeMe::ft_getVList( void ) {
+	return (this->Vlist);
 }
 
-void PmergeMe::set_time( void ) {
-	gettimeofday(&_time, NULL);
-	return ;
-}
+//Ford Johnson algorithm and Jacobsthal numbers functions for deque
 
-bool	specialSort(const std::pair<int, int>& i, const std::pair<int, int>& j) {
-	return (i.first < j.first);
-}
-
-std::deque<int> PmergeMe::fordJohnson(std::deque<int> container) {
-	int								leftover;
-	std::deque<int> 				winner;
+std::deque<int> PmergeMe::ft_fordJohnson(std::deque<int> container) {
 	std::deque<std::pair<int, int> >	pairs;
-	//cas d'arrêt
+	std::deque<int>						winner;
+	int									leftover;
+	int									a, b;
+
 	if (container.size() <= 1)
 		return std::deque<int>(container);
-
 	leftover = -1;
-	//pour chaque élément de la liste d'entrée => créer une paire selon la comparaison
 	std::deque<int>::iterator it = container.begin();
 	while (it != container.end()) {
-		int a = *it;
+		a = *it;
 		it++;
-
 		if (it == container.end()) {
 			leftover = a;
 			break;
 		}
-
-		int b = *it;
+		b = *it;
 		it++;
-
-
 		if (b > a) {
 			pairs.push_back(std::make_pair(b, a));
 		}
@@ -124,11 +69,11 @@ std::deque<int> PmergeMe::fordJohnson(std::deque<int> container) {
 
 		winner.push_back(pairs.back().first);
 	}
-	std::deque<int>result = fordJohnson(winner);
-	std::sort(pairs.begin(), pairs.end(), specialSort);
+	std::deque<int>result = ft_fordJohnson(winner);
+	std::sort(pairs.begin(), pairs.end(), ft_specialSort);
 	result.push_front(pairs[0].second);
-	for (int k = 3; Jacobsthal(k - 1) <= static_cast<int>(pairs.size()); k++)
-		binary_search(result, pairs, Jacobsthal(k), Jacobsthal(k - 1));
+	for (int k = 3; ft_Jacobsthal(k - 1) <= static_cast<int>(pairs.size()); k++)
+		ft_binary_search(result, pairs, ft_Jacobsthal(k), ft_Jacobsthal(k - 1));
 	if (leftover != -1) {
 		std::deque<int>::iterator iteratorBi = std::lower_bound(result.begin(), result.end(), leftover);
 		result.insert(iteratorBi, leftover);
@@ -136,23 +81,89 @@ std::deque<int> PmergeMe::fordJohnson(std::deque<int> container) {
 	return (result);
 }
 
-int		PmergeMe::Jacobsthal(int n) {
+int		PmergeMe::ft_Jacobsthal(int n) {
 	if (n == 0)
 		return (0);
 	else if (n == 1)
 		return (1);
 	else
-		return (Jacobsthal(n - 1) + 2 * (Jacobsthal(n - 2)));
+		return (ft_Jacobsthal(n - 1) + 2 * (ft_Jacobsthal(n - 2)));
 }
 
-void	PmergeMe::binary_search(std::deque<int>& S, std::deque<std::pair<int, int> >& pairs, int Jacobsthal, int prevJacobsthal) {
+void	PmergeMe::ft_binary_search(std::deque<int>& S, std::deque<std::pair<int, int> >& pairs, int Jacobsthal, int prevJacobsthal) {
 	for (int i = std::min(Jacobsthal - 1, static_cast<int>(pairs.size() - 1)); prevJacobsthal - 1 < i; i--) {
-		binary_insert(S, pairs[i].first, pairs[i].second);
+		ft_binary_insert(S, pairs[i].first, pairs[i].second);
 	}
 }
 
-void	PmergeMe::binary_insert(std::deque<int>& S, int Ai, int Bi) {
+void	PmergeMe::ft_binary_insert(std::deque<int>& S, int Ai, int Bi) {
 	std::deque<int>::iterator it = std::lower_bound(S.begin(), S.end(), Ai);
 	std::deque<int>::iterator iteratorBi = std::lower_bound(S.begin(), it, Bi);
 	S.insert(iteratorBi, Bi);
+}
+
+//Ford Johnson algorithm and Jacobsthal numbers functions for deque
+
+std::vector<int> PmergeMe::ft_fordJohnsonVector(std::vector<int> container) {
+	std::vector<std::pair<int, int> >	pairs;
+	std::vector<int>					winner;
+	int									leftover;
+	int									a, b;
+
+	if (container.size() <= 1)
+		return std::vector<int>(container);
+	leftover = -1;
+	std::vector<int>::iterator it = container.begin();
+	while (it != container.end()) {
+		a = *it;
+		it++;
+		if (it == container.end()) {
+			leftover = a;
+			break;
+		}
+		b = *it;
+		it++;
+		if (b > a) {
+			pairs.push_back(std::make_pair(b, a));
+		}
+		else {
+			pairs.push_back(std::make_pair(a, b));
+		}
+
+		winner.push_back(pairs.back().first);
+	}
+	std::vector<int>result = ft_fordJohnsonVector(winner);
+	std::sort(pairs.begin(), pairs.end(), ft_specialSort);
+	result.insert(result.begin(), pairs[0].second);
+	for (int k = 3; ft_Jacobsthal(k - 1) <= static_cast<int>(pairs.size()); k++)
+		ft_binarySearchVector(result, pairs, ft_Jacobsthal(k), ft_Jacobsthal(k - 1));
+	if (leftover != -1) {
+		std::vector<int>::iterator iteratorBi = std::lower_bound(result.begin(), result.end(), leftover);
+		result.insert(iteratorBi, leftover);
+	}
+	return (result);
+}
+
+
+void	PmergeMe::ft_binarySearchVector(std::vector<int>& S, std::vector<std::pair<int, int> >& pairs, int Jacobsthal, int prevJacobsthal) {
+	for (int i = std::min(Jacobsthal - 1, static_cast<int>(pairs.size() - 1)); prevJacobsthal - 1 < i; i--) {
+		ft_binaryInsertVector(S, pairs[i].first, pairs[i].second);
+	}
+}
+
+void	PmergeMe::ft_binaryInsertVector(std::vector<int>& S, int Ai, int Bi) {
+	std::vector<int>::iterator it = std::lower_bound(S.begin(), S.end(), Ai);
+	std::vector<int>::iterator iteratorBi = std::lower_bound(S.begin(), it, Bi);
+	S.insert(iteratorBi, Bi);
+}
+
+//Time functions
+
+double	PmergeMe::ft_getTime( void ) {
+	return (static_cast<double>(this->_time.tv_sec) * 1000000.0 + this->_time.tv_usec);
+}
+
+void PmergeMe::ft_setTime( void ) {
+	gettimeofday(&this->_time, NULL);
+	return ;
 }
--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: lomont <lomont@student.42lehavre.fr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 16:57:27 by lomont            #+#    #+#             */
-/*   Updated: 2026/02/14 05:31:41 by lomont           ###   ########.fr       */
+/*   Updated: 2026/02/15 03:18:18 by lomont           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,36 +18,64 @@
 #include <climits>
 #include <cstdlib>
 #include <cstring>
+#include <sys/time.h>
 #include <iostream>
 #include <deque>
+#include <vector>
 #include <algorithm>
-#include <sys/time.h>
 #include <iomanip>
 
 class PmergeMe
 {
 	private:
-		int sav;
-		struct timeval _time;
-		std::deque<int> list;
-		std::deque<std::pair<int, int> > winners;
+		std::deque<int> 	list;
+		std::vector<int> 	Vlist;
+		struct timeval 		_time;
 	public:
+		//canonical form
 		PmergeMe( int argc, char** argv );
 		~PmergeMe( void );
-		std::deque<int> fordJohnson(std::deque<int> container);
-		std::deque<int>& getList( void );
-		void	printResult(void);
-		int		Jacobsthal(int n);
-		void	binary_search(std::deque<int>& S, std::deque<std::pair<int, int> >& pairs, int jacobstal, int leftover);
-		void	binary_insert(std::deque<int>& S, int Ai, int Bi);
-		double	get_time( void );
-		void set_time( void );
+
+		//Getters
+		std::deque<int>&			ft_getList( void );
+		std::vector<int>&			ft_getVList( void );
+		double						ft_getTime( void );
+
+		//Setters
+		void						ft_setTime( void );
+
+		//Members function
+		int							ft_Jacobsthal(int n);
+
+		//for deque
+		void						ft_binary_search(std::deque<int>& S, std::deque<std::pair<int, int> >& pairs, int jacobstal, int leftover);
+		void						ft_binary_insert(std::deque<int>& S, int Ai, int Bi);
+		std::deque<int> 			ft_fordJohnson(std::deque<int> container);
+
+		//for vector
+		void						ft_binaryInsertVector(std::vector<int>& S, int Ai, int Bi);
+		void						ft_binarySearchVector(std::vector<int>& S, std::vector<std::pair<int, int> >& pairs, int Jacobsthal, int prevJacobsthal);
+		std::vector<int>			ft_fordJohnsonVector(std::vector<int> container);
+
 };
 
-int		parseNumbers( const char* str);
-bool	hasOverflow(double n);
-bool	isNumber(const char* str);
-void	addNumber(std::deque<int>* c, int n);
-void	printError(std::string Error);
+//Utils functions
+
+int									ft_parseNumbers( const char* str);
+bool								ft_hasOverflowed(double n);
+bool								ft_isNumber(const char* str);
+bool								ft_checkDuplicates(std::deque<int> & result);
+bool								ft_specialSort(const std::pair<int, int>& i, const std::pair<int, int>& j);
+void								ft_addNumber(std::deque<int>* c, std::vector<int>* Vc, int n);
+void								ft_printError(std::string Error);
+void								ft_printBeforeSorted(char** argv);
+
+template <typename T>
+void								ft_performCalculationAndPrintIt(T& a, char** argv, int listLen, bool isDeque);
+
+template <typename T>
+void								ft_printAfterSorted(T& container);
+
+#include "PmergeMe.tpp"
 
 #endif
